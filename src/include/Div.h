267acd66d1b341542d61ada0e046b3737a39c291
Lestine <lestine0056@qq.com>
2026-06-04 15:17:36 +0800

    feat(src): Div以及其子类新增文本显示功能，支持宽字符（Unicode）文本渲染 (#10)
    feat(src): Div以及其子类新增文本显示功能，支持宽字符（Unicode）文本渲染

--- src/include/Div.h
+++ src/include/Div.h
@@ -11,15 +11,18 @@ public:
     Div(int x, int y, int w, int h);
     Div(const std::string& className, int w = 100, int h = 30);
     Div(int x, int y, int w, int h, const std::string& className);
+	Div(const std::string& className, const std::wstring& text, int w = 100, int h = 30);
 
     void draw() override;
 
     void setClassName(const std::string& className);
     const std::string& className() const;
     void applyClassName(const std::string& className);
+    void setText(const std::wstring& text);
 
 private:
     std::string m_className;
+    std::wstring m_text;
 };
 
 } // namespace IrisGUI
